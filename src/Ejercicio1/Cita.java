ab7efd9c92b7938011734ebfbee74403d0d15aab
albertDAW <albertalonsoandreu@gmail.com>
2016-09-21 21:12:49 +0200

    Inicio

---
+++ src/Ejercicio1/Cita.java
@@ -0,0 +1,44 @@
+package Ejercicio1;
+
+
+/**
+ * Created by Albert on 21/09/2016.
+ */
+public class Cita {
+    public String motiu;
+    public String hora_fi;
+    public String hora_inici;
+    public String contingut;
+
+    public String getMotiu() {
+        return motiu;
+    }
+
+    public void setMotiu(String motiu) {
+        this.motiu = motiu;
+    }
+
+    public String getHora_fi() {
+        return hora_fi;
+    }
+
+    public void setHora_fi(String hora_fi) {
+        this.hora_fi = hora_fi;
+    }
+
+    public String getHora_inici() {
+        return hora_inici;
+    }
+
+    public void setHora_inici(String hora_inici) {
+        this.hora_inici = hora_inici;
+    }
+
+    public String getContingut() {
+        return contingut;
+    }
+
+    public void setContingut(String contingut) {
+        this.contingut = contingut;
+    }
+}
---
+++ src/Ejercicio1/Pagina.java
@@ -0,0 +1,34 @@
+package Ejercicio1;
+
+/**
+ * Created by Albert on 21/09/2016.
+ */
+public class Pagina {
+    public String mes;
+    public Integer dia;
+    public boolean esFestiu;
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public Integer getDia() {
+        return dia;
+    }
+
+    public void setDia(Integer dia) {
+        this.dia = dia;
+    }
+
+    public boolean isEsFestiu() {
+        return esFestiu;
+    }
+
+    public void setEsFestiu(boolean esFestiu) {
+        this.esFestiu = esFestiu;
+    }
+}
