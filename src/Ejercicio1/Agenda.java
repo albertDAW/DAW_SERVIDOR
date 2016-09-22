0eb8b65f7943bc03c350f9c027ab7ec38701bdb4
albertDAW <albertalonsoandreu@gmail.com>
2016-09-22 16:12:44 +0200

    Inicio

---
+++ src/Ejercicio1/Agenda.java
@@ -0,0 +1,23 @@
+package Ejercicio1;
+
+import com.sun.javafx.scene.control.skin.PaginationSkin;
+
+import java.util.List;
+
+/**
+ * Created by Albert on 21/09/2016.
+ */
+public class Agenda {
+    public List<Pagina> paginas;
+    public int ann;
+    public int getAnn() {
+        return ann;
+    }
+    public void setAnn(int ann) {
+        this.ann = ann;
+    }
+    public void pasar_pagina(boolean avanzar) {
+        if (avanzar){
+        }
+    }
+}
---
+++ src/Ejercicio1/main.java
@@ -0,0 +1,7 @@
+package Ejercicio1;
+
+/**
+ * Created by Albert on 21/09/2016.
+ */
+public class main {
+}
