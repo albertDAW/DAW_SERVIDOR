bf174cc08edb63eed0945a4ebdbb2337320c7c62
albertDAW <albertalonsoandreu@gmail.com>
2016-09-23 17:12:32 +0200

    Inicio

---
+++ src/Ejercicio2/main.java
@@ -0,0 +1,16 @@
+package Ejercicio2;
+
+/**
+ * Created by Albert on 23/09/2016.
+ */
+public class main {
+
+    public static void main(String args[]){
+        try {
+
+        }
+        catch (Exception e){
+
+        }
+    }
+}
