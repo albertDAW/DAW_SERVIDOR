ff7aa9b7de13e48992c476c12a81a50f7c702088
albertDAW <albertalonsoandreu@gmail.com>
2016-09-23 17:26:55 +0200

    Inicio

---
+++ src/Ejercicio2/Borrar.java
@@ -0,0 +1,10 @@
+package Ejercicio2;
+
+/**
+ * Created by Albert on 23/09/2016.
+ */
+public class Borrar {
+    protected void finalize() throws Throwable {
+        System.out.println("Barcos y putas");
+    }
+}
--- src/Ejercicio2/main.java
+++ src/Ejercicio2/main.java
@@ -7,7 +7,9 @@ public class main {
 
     public static void main(String args[]){
         try {
-
+            Borrar objeto = new Borrar();
+            objeto = null;
+            System.gc();
         }
         catch (Exception e){
 
