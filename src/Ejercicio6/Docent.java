43dfe58f7a9cc396ca27c3f6093287071f8c8191
albertDAW <albertalonsoandreu@gmail.com>
2016-10-04 17:41:14 +0200

    Interficies 0.1

--- src/Ejercicio6/Docent.java
+++ src/Ejercicio6/Docent.java
@@ -4,10 +4,12 @@ package Ejercicio6;
  * Created by Albert on 04/10/2016.
  */
 public class Docent extends Persona implements Treballador {
+    private String ccc;
+    
     public void pagaAlTreballador(Treballador treballador){
         String ccc = treballador.getcCC();
     }
     public String getcCC(){
-        return "";
+        return this.ccc;
     }
 }
